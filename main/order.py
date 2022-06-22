e1b857410d4191259603867b91c3c96dd565b611
jonghoeJeon <jonghoeJeon>
2022-06-22 21:02:59 +0900

    order 수정 추가

--- main/order.py
+++ main/order.py
@@ -1,5 +1,6 @@
 import datetime
 from tracemalloc import start
+from webbrowser import get
 from flask import Blueprint, jsonify, make_response, request
 from pymysql import Connection
 import pymysql
@@ -20,9 +21,9 @@ def createOrder():
         for order in orderData:
             now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
             sql = """ 
-                insert into tb_order_history (store_id, item, color, size, quantity, created_date, last_modified_date) values(%s, %s, %s, %s, %s, %s, %s)
+                insert into tb_order_history (store_id, user_id, item, color, size, quantity, created_date, last_modified_date) values(%s, %s, %s, %s, %s, %s, %s, %s)
             """
-            data = (order['store_id'] , order['item'], order['color'], order['size'], order['quantity'], now, now)
+            data = (order['store_id'], order['user_id'], order['item'], order['color'], order['size'], order['quantity'], now, now)
             conn = pymysql.connect(host = 'meta-soft.iptime.org', # 디비 주소 //localhost
                                     user = 'root',                 # 디비 접속 계정
                                     password = 'root',             # 디비 접속 비번
@@ -77,6 +78,42 @@ def Order():
         row = cursor.fetchall()
         conn.commit()
         return make_response(jsonify({'result': 'success', 'data': row}), 200)
+    except Exception as e:
+        print(e)
+        return 'false'
+    finally:
+        cursor.close() 
+        conn.close()
+
+
+@blueprint_order.route("/save-order-list", methods=['POST'])
+def modifyOrderList():
+    conn = None
+    cursor = None
+    getData = request.get_json()
+    print(getData[0])
+    try:    
+        for order in getData:
+            now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+            sql = """
+                insert into tb_order_history (id, store_id, user_id, item, color, size, quantity, available_status, comment, created_date, last_modified_date) 
+                values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON DUPLICATE KEY UPDATE `available_status` = VALUES(`available_status`), `comment` = VALUES(`comment`), `last_modified_date` = VALUES(`last_modified_date`)
+                """
+            
+            data = (order['id'], order['store_id'], order['user_id'], order['item'], order['color'], order['size'], order['quantity'], order['available_status'], order['comment'],  now, now)
+            conn = pymysql.connect(host = 'meta-soft.iptime.org', # 디비 주소 //localhost
+                                    user = 'root',                 # 디비 접속 계정
+                                    password = 'root',             # 디비 접속 비번
+                                    db = 'temp',               # 데이터 베이스 이름
+                                    port = 53306,                  # 포트
+                                    charset = 'utf8')
+            cursor = conn.cursor()
+            print(cursor)
+            cursor.execute(sql, data)
+            conn.commit()
+            # return make_response(jsonify({'result': 'success', 'data': row}), 200)
+        return 'true'
     except Exception as e:
         print(e)
         return 'false'
