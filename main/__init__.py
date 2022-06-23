a4c6a47c657772b15cfa1645ede8659a54b51ab4
y <yeoungsul@gmail.com>
2022-06-23 15:56:31 +0900

    python-dotenv 추가 (가상환경에서 .flaskenv 설정)

--- main/__init__.py
+++ main/__init__.py
@@ -5,14 +5,12 @@ from . import order
 from . import store
 import pymysql
 from flask_cors import CORS, cross_origin
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 Cors = CORS(app)
 CORS(app, resources={r'/*': {'origins': '*'}},CORS_SUPPORTS_CREDENTIALS = True)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
 
 app.register_blueprint(User.blueprint_user)
 app.register_blueprint(login.blueprint_login)
